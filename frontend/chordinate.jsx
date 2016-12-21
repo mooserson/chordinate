9513a9cef2e951d4194ed85d7db567998eb49e53
PaulCathey <pmcathey@gmail.com>
2016-12-20 23:44:27 -0600

    adds object.values shim

--- frontend/chordinate.jsx
+++ frontend/chordinate.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import values from 'object.values';
+
+if (!Object.values) {
+  values.shim();
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
