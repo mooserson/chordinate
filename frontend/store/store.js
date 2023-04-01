4fb752509ed36918f42d24ddf701ddc9554bfcba
mooserson <pmcathey@gmail.com>
2023-04-01 16:00:36 -0500

    fixes redux devtools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,18 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import RootReducer from '../reducers/root_reducer.js';
 import RootMiddleware from '../middleware/root_middleware.js';
 
 // adds Redux dev tools if in dev env
-const composeEnhancers =
-  process.env.NODE_ENV === 'development' && typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(RootMiddleware)
-    : RootMiddleware;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preLoadedState = {}) => (
   createStore(
     RootReducer,
     preLoadedState,
-    composeEnhancers
+    composeEnhancers(RootMiddleware)
   )
 );
 
